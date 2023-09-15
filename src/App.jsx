1c8aea0484af3ed76aafbd21fa3a756fe4113288
Naim Ahmad <naimahmad201103@gmail.com>
2023-09-15 07:06:59 +0600

    show alert if this course is already selected

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Swal from 'sweetalert2'
 import './App.css'
 import Cart from './components/cart/Cart'
 import Courses from './components/courses/Courses'
@@ -7,9 +8,16 @@ function App() {
   const [selected, setSelected] = useState([])
 
   const selectHandler = (course) => {
-    setSelected([...selected, course])
+    const isExist = selected.find(item => item.id === course.id)
+    if (!isExist) {
+      setSelected([...selected, course])
+    } else {
+      Swal.fire({
+        icon: 'warning',
+        title: 'This course has already been added to your cart!'
+      })
+    }
   }
-console.log(selected)
   return (
     <>
       <header className='max-w-[1200px] mx-auto px-8'>
